532eae71a7c377d7cdc3db8a31df11cb398efba6
Ghost-Rog <ghostrog@mail.ru>
2020-06-20 22:35:44 +0300

    Initial commit123

---
+++ src/Game/DotCom.java
@@ -0,0 +1,36 @@
+package Game;
+
+import java.util.ArrayList;
+
+public class DotCom {
+
+    private ArrayList<String> locationCells;
+    private String name;
+
+    public void setLocationCells(ArrayList<String> locs) {
+        this.locationCells = locs;
+    }
+
+    public void setName(String n) {
+        this.name = n;
+    }
+
+    public String checkYourself(String userInput) {
+
+        String result = "Мимо";
+
+        int index = locationCells.indexOf(userInput);
+
+        if (index >= 0) {
+            locationCells.remove(index);
+
+            if (locationCells.isEmpty()) {
+                result = "Потопил";
+                System.out.println("Ой! Вы потопили " + name);
+            } else {
+                result = "Попал";
+            }
+        }
+        return result;
+    }
+}
